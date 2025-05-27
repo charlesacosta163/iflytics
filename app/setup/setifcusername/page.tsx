62a02b283a4c626e20881af7a8c58de1ec4b9748
Charles Acosta <128003482+charlesacosta163@users.noreply.github.com>
2025-05-27 00:15:39 -0400

    Delete app/setup directory

--- app/setup/setifcusername/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import SetIFCUsernameForm from '@/components/dashboard-ui/set-ifc-username-form'
-const SetIFCUsernamePage = () => {
-  return (
-    <SetIFCUsernameForm />
-  )
-}
-
-export default SetIFCUsernamePage
