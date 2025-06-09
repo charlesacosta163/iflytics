6071b7894d8e408dd9f73f2f6c956186c0768723
Charles Acosta <charlesacosta163@gmail.com>
2025-06-09 16:42:20 -0400

    redeploy push

--- components/dashboard-ui/flights/flights-routes.tsx
+++ components/dashboard-ui/flights/flights-routes.tsx
@@ -124,7 +124,7 @@ const FlightsRoutes = async ({ flights }: { flights: Flight[] }) => {
                             </div>
                             <div className="flex items-center gap-2">
                             <div className="w-3 h-3 bg-red-500 rounded-full"></div>
-                            <span className="text-xs text-gray-600">Long (>6h)</span>
+                            <span className="text-xs text-gray-600">Long (6h+)</span>
                             </div>
                     </div>
                   </div>
